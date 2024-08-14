4203276b21dab5134a7abeefc5d6c0ff048125d4
pcrab <pcrab@pcrab.xyz>
2024-08-14 23:26:16 +0800

    feat: add config flags

--- conf.d/fnm.fish
+++ conf.d/fnm.fish
@@ -1,15 +1,16 @@
 # fnm
 set --query FNM_PATH || set FNM_PATH "$HOME/.local/share/fnm"
+
 if [ -d "$FNM_PATH" ]
     set PATH "$FNM_PATH" $PATH
-    fnm env --use-on-cd --shell fish | source
+    fnm env --version-file-strategy=recursive --corepack-enabled --use-on-cd --shell fish | source
 end
 
 function _fnm_install --on-event fnm_install
     if not test -d "$FNM_PATH"
         set PATH "$FNM_PATH" $PATH
         install_fnm
-        fnm env --use-on-cd --shell fish | source
+        fnm env --version-file-strategy=recursive --corepack-enabled --use-on-cd --shell fish | source
     end
 end
 
